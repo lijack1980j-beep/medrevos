cde427b2acd3e8816541a726abe300c1c93a2c04
lijack1980j-beep <lijack1980j@gmail.com>
2026-03-18 05:52:03 +0500

    update project

--- components/GlitchSignalFooter.tsx
+++ components/GlitchSignalFooter.tsx
@@ -73,7 +73,7 @@ export function GlitchSignalFooter() {
               onMouseEnter={() => setIconHover(true)}
               onMouseLeave={() => setIconHover(false)}
               aria-label="Instagram"
-              onClick={() => window.open('https://www.instagram.com/meds.el_hirech/', '_blank')}
+              onClick={() => window.open('https://www.instagram.com/glitch_signal/', '_blank')}
             >
               <InstagramIcon />
             </button>
